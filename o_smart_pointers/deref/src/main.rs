bc5bbf160d411e5f8b4d535e87a65916afb10da2
Davor Šercer <davor.sercer@gmail.com>
2025-06-29 15:26:16 +0200

    smart pointers finisherd

---
+++ o_smart_pointers/deref/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+// fn main() {
+//     let x = 5;
+//     let y = &x;
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
